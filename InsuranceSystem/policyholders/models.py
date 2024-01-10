13112bc74cc1306690ed50e870200ec7fca27761
Yun-Hsin Chen <chen_yun_hsin@amastek.com>
2024-01-10 21:29:55 +0800

    feat: 增加model和預設資料

--- InsuranceSystem/policyholders/models.py
+++ InsuranceSystem/policyholders/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.conf import settings
+from django.core.validators import MaxLengthValidator
 
-# Create your models here.
+
+class Policyholder(models.Model):
+    id = models.AutoField(primary_key=True)
+    # mockup 最大長度為10
+    code = models.CharField(max_length=10,unique=True)
+    # 目前台灣最長人名為25個字
+    name = models.CharField(max_length=25)
+    registration_date = models.DateTimeField(auto_now_add=True)
+    # 第一位保險人的介紹人是null?
+    introducer = models.ForeignKey('policyholders.Policyholder', on_delete=models.CASCADE,  related_name='policyholders',null=True)
